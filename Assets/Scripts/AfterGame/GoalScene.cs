33a7d9d931e73579f5d9f72324b070350c851a50
mashi97856 <akihitomashi@gmail.com>
2026-06-05 11:38:34 +0900

    ポイント集計画面の追加

---
+++ Assets/Scripts/AfterGame/GoalScene.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class GoalScene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            SceneManager.LoadScene("PointCountingScene");
+        }
+    }
+}
---
+++ Assets/Scripts/AfterGame/PointGameManagment.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+
+public class PointGameManagment : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
