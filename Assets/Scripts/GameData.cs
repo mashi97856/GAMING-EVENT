77886ba28abfd2594de7a95e37fa3953a271d2aa
mashi97856 <akihitomashi@gmail.com>
2026-06-27 19:54:58 +0900

    変更

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -6,5 +6,5 @@ public static class GameData
     public static int currentPlayer = 0;
     public static bool bombGameFinished = false;
     public static bool bombExploded = false; // 爆弾が爆発したかどうか
-    public static int bombExplodedPlayer = -1; // 爆発したプレイヤー（0=A, 1=B）
+    public static int bombExplodedPlayer = -1; 
 }
